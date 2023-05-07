c2fa602adb88b6601d5d662fbce506e136ec4253
DerpyMissile <hmai015@ucr.edu>
2023-05-07 14:44:27 -0700

    merge zijun branch

--- main.py
+++ main.py
@@ -8,32 +8,32 @@ def main():
     print("Type '1' to use a default puzzle, or '2' to enter your own puzzle.")
     choice = int(input())
 
-if (choice == 1):
-    print("You have chosen to use a default puzzle.")
-    puzzle = [1, 2, 3, 4, 8, 0, 7, 6, 5]
-    temp = Problems.Problem(puzzle)
-    start = time.time()
-    node = Problems.general_alg(temp)
-    end = time.time()
-    #print(puzzle)   # temp debug
-    print("Time to finish: {}".format(end - start))
+    if (choice == 1):
+        print("You have chosen to use a default puzzle.")
+        puzzle = [1, 2, 3, 4, 8, 0, 7, 6, 5]
+        temp = Problems.Problem(puzzle)
+        start = time.time()
+        node = Problems.general_alg(temp)
+        end = time.time()
+        #print(puzzle)   # temp debug
+        print("Time to finish: {}".format(end - start))
 
-elif (choice == 2):
-    print("Enter your puzzle, use a zero to represent the blank.")
-    puzzle = [int(item) for item in input("Enter the first row, use space or tabs between numbers\n").split()]
-    puzzle += [int(item) for item in input("Enter the second row, use space or tabs between numbers\n").split()]
-    puzzle += [int(item) for item in input("Enter the second row, use space or tabs between numbers\n").split()]
-    for x in puzzle:
-        check = x
-        for y in puzzle:
-            if (check==y):
-                print("Invalid puzzle.")
-                exit(0)
-                break
-        print(puzzle)   # temp debug
-    else:
-        print("Invalid option.")
-        exit(0)
+    elif (choice == 2):
+        print("Enter your puzzle, use a zero to represent the blank.")
+        puzzle = [int(item) for item in input("Enter the first row, use space or tabs between numbers\n").split()]
+        puzzle += [int(item) for item in input("Enter the second row, use space or tabs between numbers\n").split()]
+        puzzle += [int(item) for item in input("Enter the second row, use space or tabs between numbers\n").split()]
+        for x in puzzle:
+            check = x
+            for y in puzzle:
+                if (check==y):
+                    print("Invalid puzzle.")
+                    exit(0)
+                    break
+            print(puzzle)   # temp debug
+        else:
+            print("Invalid option.")
+            exit(0)
 
     puzzle2 = []
     puzzle2 = (puzzle)
